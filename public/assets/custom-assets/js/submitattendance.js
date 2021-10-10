bcbf8ac8d61cf5bb1014978c70ac76c3d1caa4a3
asifmujtoba <asifmujtoba@gmail.com>
2021-10-10 13:29:59 +0800

    Update submitattendance.js

--- public/assets/custom-assets/js/submitattendance.js
+++ public/assets/custom-assets/js/submitattendance.js
@@ -5,12 +5,13 @@ async function submitAttendance(){
     if(!check_attendance && check.text() == 'Check In' || check.text() == 'Check Out'){
         await $.ajax({url: '/api/users/registerattendance'});
         if(check.text() == "Check Out"){
-            text = 'Checked Out';
+            text = 'Checked Out'; 
         }
         else if(check.text() == 'Check In'){
-            text = 'Check In'
+            text = 'Check Out';
+            
         }
-       check.text(text); 
+        check.text(text); 
     }
     else{
         return;
